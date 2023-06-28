a80ac5e636c6c834d77dbbe3be1267a7bd44d3cb
Jesse Pinheiro <jessebpinheiro@hotmail.com>
2023-06-28 05:48:12 -0300

    pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const isGithubActionsPages = process.env.GITHUB_ACTIONS_PAGES || false;
 
 let assetPrefix = ''
 let basePath;
 
-if (isGithubActions) {
+if (isGithubActionsPages) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
   assetPrefix = `/${repo}/`
